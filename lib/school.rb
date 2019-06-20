a28a19ae72a241a57d1cb2a90d24470d61be552c
Kimberly Rouse <ksrouse94@gmail.com>
2019-06-20 23:01:36 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,40 @@
 class School
   def initialize(name)
     @name = name 
+    @roster = {}
   end  
 
-  roster = {}
+  attr_reader :name, :roster, :grade, :student_name
   
+  def add_student(student_name, grade)
+    @student_name = student_name
+    @grade = grade
   
-end
+    #@roster[@grade] = []
+    #@roster[@grade] << @student_name
+    
+    keys = @roster.keys
+    
+    if !keys.include?(@grade) 
+      @roster[@grade] = []
+      @roster[@grade] << @student_name
+    else
+      @roster[@grade] << @student_name
+    end  
+  
+  end  
+
+  def grade(grade)
+    @grade = grade
+    
+    return @roster[@grade]
+    
+  end
+
+  def sort 
+    @roster.each do |grade, name_array|
+      name_array.sort!
+    end  
+  end  
+  
+end  
